a66be0035d452263aba9bb340c89c322808966e6
Александр Захаров <zaher0861@yandex.ru>
2021-12-02 22:34:20 +0300

    Добавьте файлы проекта.

---
+++ Lesson_7.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Чтобы найти площадь и объем куба введите длину его ребра: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            if (a>0)
+            {
+                double V, S;
+                CalcCube(a, out V, out S);
+                Console.Write("Площадь куба равна {0:F2}, а объем куба равен {1:F2}", S, V);
+            }
+            else
+            {
+                Console.WriteLine("Нельзя построить куб со стороной <=0!");
+            }
+            Console.ReadKey();
+        }
+        static void CalcCube(double a, out double V, out double S)
+        {
+            V = Math.Pow(a, 3);
+            S = Math.Pow(a, 2) * 6;
+        }
+
+    }
+}
---
+++ Lesson_7/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение стороны х первого треугольника: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите значение стороны y первого треугольника: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите значение стороны z первого треугольника: ");
+            double z = Convert.ToDouble(Console.ReadLine());
+            double S1 = CalcSquare(x, y, z);
+            // Здесь мы проверяем, можно ли построить первый трегольник с такими сторонами 
+            if (S1 > 0 && x > 0 && y > 0 && z > 0)
+            {
+                Console.WriteLine("Площадь первого треугольника = {0:F2}", CalcSquare(x, y, z));
+                Console.WriteLine();
+                Console.Write("Введите значение стороны х второго треугольника: ");
+                x = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите значение стороны y второго треугольника: ");
+                y = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите значение стороны z второго треугольника: ");
+                z = Convert.ToDouble(Console.ReadLine());
+                double S2 = CalcSquare(x, y, z);
+                // Здесь мы проверяем, можно ли построить второй трегольник с такими сторонами 
+                if (S2 > 0 && x > 0 && y > 0 && z > 0)
+                {
+                    Console.WriteLine("Площадь второго треугольника = {0:F2}", S2);
+                    Console.WriteLine();
+                    // Здесь мы проверяем на то, равны ли наши треугольники между собой
+                    if (S1 == S2)
+                    {
+                        Console.WriteLine("Вы задали одинаковые треугольники!");
+                    }
+                    else
+                    {
+                        if (S1 > S2)
+                        {
+                            Console.WriteLine("Площадь первого треугольника больше чем площадь второго!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Площадь второго треугольника больше чем площадь первого!");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Из таких сторон нельзя построить треугольник!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Из таких сторон нельзя построить треугольник!");
+            }
+            Console.ReadKey();
+        }
+        static double CalcSquare(double x, double y, double z)
+        {
+            double p = (x + y + z) / 2;
+            double S = Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+            return S;
+        }
+    }
+}
